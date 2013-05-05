9c8b8f1f2ba3cb6200cce198e956c383dbffe6f0
Jim Herd <jtherd@gmail.com>
2013-05-05 22:57:56 +0000

    Added SRF08 test routine

---
+++ srf08_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+from srf08 import SRF08
+import time
+
+#=====================================================
+# SRF08_test : exercise SRF08 class
+#
+
+srf08 =  SRF08(0x70, debug=True)
+
+print "SRF08 test started"
+
+# srf08.write_reg(2, 140)
+# srf08.write_reg(1, 31)
+for x in range(0, 9):
+  srf08.write_reg(0, 0x51)
+  time.sleep(1)
+  dist = srf08.read_echo(1)
+  print "Distance = %d" % dist
+
