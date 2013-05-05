276451a963b4e74b73845043c875e2198affe7e7
Jim Herd <jtherd@gmail.com>
2013-05-05 21:19:40 +0000

    Added MD03 module code

---
+++ md03.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+#========================================================
+# MD03 24V 20A H-bridge motor driver
+#
+# Author : Jim Herd
+# Date : May 2013
+#
+# Notes
+#	Uses Adafruit_I2C module
+
+from Adafruit_I2C import Adafruit_I2C
+
+class MD03 :
+  i2c = None
+
+# constructor
+  def __init__(self, address=0xB0, debug=False):
+    self.i2c = Adafruit_I2C(address)
+    self.address = address
+    self.debug = debug
+
+  def set_speed(self, speed): 
+    if ((speed < 0 ) | (speed > 100)):
+      if (self.debug):
+	print "Speed %d outwith range 0 to 100%" % speed
+	return -1 
+    spd = ((speed * 255)/100)
+    self.i2c.write8(2, spd)
+    return 0
+
+  def set_accel(self, accel):
+    if ((accel < 0) | (accel > 100)):
+      if (self.debug):
+        print "Acceleration %d outwith range 0 to 100%" % accel
+    acc = 255 - ((accel * 255)/100)
+    self.i2c.write8(3, acc)
+    return 0
+
+  def stop(self):
+    self.i2c.write8(0, 0)
+
+  def move_forward(self):
+    self.i2c.write(0, 1)
+
+  def move_reverse(self):
+    self.i2c.write8(0, 2)
+
+  def read_reg(self, reg):
+    return  self.i2c.readU8(reg)
+
---
+++ md03_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+from md03 import MD03
+import time
+
+#=====================================================
+# MD03_test : exercise MD03 class
+#
+
+md03 =  MD03(0x51, debug=True)
+
+print "MD03 test started"
+
+md03.set_speed(50)
+md03.set_accel(100)
+md03.move_forward()
+time.sleep(3)
+md03.stop()
+time.sleep(3)
+print "Software revision = %d" % md03.read_reg(7)
